873a2722789374efcd218158fc7adeb6f8aa7322
nvoff <nikita.voxmincev@mail.ru>
2024-05-16 13:42:27 +0300

    Create generators.py

---
+++ generators.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+import httpx 
+from openai import AsyncOpenAI
+
+load_dotenv()
+client = AsyncOpenAI(api_key=os.getenv('AI_TOKEN'),
+                     http_client=httpx.AsyncClient(
+                     proxies=os.getenv('PROXY'), 
+                     transport=httpx.HTTPTransport(local_address="0.0.0.0")   
+                     ))
+
+async def gpt4(question):
+    response = await client.chat.completions.create(
+        messages=[{"role": "user", 
+                   "content": str(question)}],
+        model="gpt-4o",
+        stream=False  # Добавлен аргумент stream
+    )
+    return response
