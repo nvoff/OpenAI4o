ea1180b21498a1fadeda6bddd5b4dfc645f2a378
nvoff <nikita.voxmincev@mail.ru>
2024-05-16 13:41:33 +0300

    Create run.py

---
+++ run.py
@@ -0,0 +1,20 @@
+import os
+import asyncio
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+
+from app.handlers import router
+
+async def main():
+    load_dotenv()
+    bot = Bot(token=os.getenv('TG_TOKEN'))
+    dp = Dispatcher()
+    dp.include_routers(router)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print ('Бот выключен')
